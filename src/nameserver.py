5afd8c0684f4defe7e414eb34dcbad8c104da5a0
Renato Gentil <renatorodrigo_87@hotmail.com>
2021-05-14 00:48:11 +0100

    updating naming

--- src/nameserver.py
+++ src/nameserver.py
@@ -4,10 +4,14 @@ import socket
 HOST = '127.0.0.1'
 PORT = 53
 
-def socket_definition():
+def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((HOST, PORT))
 
     while True:
         data, addr = sock.recvfrom(512)
         print(data)
+
+
+if __name__ == "__main__":
+    main()
