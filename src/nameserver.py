c80ffcbce739af55dd3d8ed9038210df557d00da
Renato Gentil <renatorodrigo_87@hotmail.com>
2021-05-16 23:20:01 +0100

    adding response

--- src/nameserver.py
+++ src/nameserver.py
@@ -10,8 +10,16 @@ def main():
 
     while True:
         data, addr = sock.recvfrom(512)
-        print(data)
+        response = dns_response(data)
+        sock.sendto(response, addr)
 
+def dns_response(data):
+    TransactionID = data[0:2]
+    TID = ''
+    for byte in TransactionID:
+        TID +=hex(byte)[2:]
+    print(TID)
 
+        
 if __name__ == "__main__":
-    main()
+        main()
